2099f3416dfd3fef35a32ef5b349fec30ce38327
zizo-26 <abdelazizbon24@gmail.com>
2026-06-13 14:16:48 +0200

    working with class list in the DOM

--- script.js
+++ script.js
@@ -4,11 +4,28 @@
 const modal=document.querySelector(".modal");
 const overlay=document.querySelector(".overlay");
 const closeBtn=document.querySelector(".close-modal");
-const openBtn=document.querySelectorAll(".show-modal");
+let openBtn=document.querySelectorAll(".show-modal");
 
 console.log(openBtn);
 
 for(let i=0 ;i<openBtn.length; i++){
 
     console.log(openBtn[i].textContent);
+    openBtn[i].addEventListener('click',function(){
+
+        // for class list we can pass more then one string .....
+
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
 }
+
+const closeModal=function(){
+
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+closeBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click',closeModal)
