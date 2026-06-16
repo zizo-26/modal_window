f2334a8e5fdc3ad54d62cd26c34b5723a6fb09e7
zizo-26 <abdelazizbon24@gmail.com>
2026-06-16 19:46:17 +0200

    close function

--- script.js
+++ script.js
@@ -26,7 +26,12 @@ const closeModal=function(){
     overlay.classList.add('hidden');
 }
 
-closeBtn.addEventListener('click', closeModal)
+closeBtn.addEventListener('click', function(){
+
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+)
 
 overlay.addEventListener('click',closeModal)
 
